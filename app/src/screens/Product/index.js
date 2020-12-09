2c7122501667fb2610019fa6df09eb2fc7086950
Rafael Makaha <rafael.makaha@gmail.com>
2020-12-09 03:47:14 -0300

    Update Product page.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- app/src/screens/Product/index.js
+++ app/src/screens/Product/index.js
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { createProduct, deleteProductById, getProductById, updateProduct } from '../../services';
+import { useParams, useHistory } from 'react-router-dom';
+
+import TextField from '@material-ui/core/TextField';
+import { ButtonsWrapper, Container, Form, Typography, ButtonSaveContainer } from './style';
+import { Button } from '@material-ui/core';
 
 const Product = () => {
+  const [product, setProduct] = useState({});
+
+  const params = useParams();
+  const history = useHistory();
+
+  const handlePush = () => {
+    history.push('/');
+  }
+
+  const handleSubmit = (type) => () => {
+    const actionButton = {
+      create: (product) => {console.log(product);createProduct(product); history.push('/');},
+      update: updateProduct,
+      delete: (product) => {deleteProductById(product); history.push('/')},
+    }
+    if (actionButton[type]) 
+      actionButton[type](product)
+      .then(console.log)
+  }
+
+  useEffect(() => {
+    if ('id' in params)
+      getProductById(params)
+      .then(setProduct)
+      .catch(console.log)
+  },[]);
+
   return(
-    <div>Product</div>
+    <Container>
+        <Typography variant="h3" gutterBottom>
+          {'id' in params ? 'Editar Produto' : 'Inserir Produto'}
+        </Typography>
+        <Form noValidate autoComplete="off">
+          <TextField id="outlined-basic" value={product.name} label="Name" variant="outlined" />
+          <TextField id="outlined-basic" value={product.description} label="Description" variant="outlined" />
+          <TextField id="outlined-basic" value={product.price} label="Price" variant="outlined" type='number' />
+          <ButtonsWrapper>
+            <Button variant="contained" color="primary" onClick={handlePush}>
+              Back
+            </Button>
+            <ButtonSaveContainer>
+              {'id' in params && 
+              <Button variant="contained" color="secondary" onClick={handleSubmit('delete')}>
+                Delete
+              </Button>
+              }
+              <Button variant="contained" color="primary" onClick={handleSubmit('id' in params ? 'update' : 'create')}>
+                Save
+              </Button>
+            </ButtonSaveContainer>
+          </ButtonsWrapper>
+      </Form>
+    </Container>
   )
 }
 
--- app/src/screens/Product/style.js
+++ app/src/screens/Product/style.js
@@ -1,2 +1,31 @@
-import {} from 'styled-components';
+import styled from 'styled-components';
+import { Typography as MaterialTypography } from '@material-ui/core';
 
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2em;
+  width: 100%;
+  max-width: 800px;
+  margin: auto;
+`
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 1.2em;
+  justify-content: space-between;
+`
+
+export const Container = styled.div`
+  margin-top: 1.2em;
+`
+
+export const Typography = styled(MaterialTypography)`
+  margin-top: 1.2em;
+  text-align: center;
+`
+export const ButtonSaveContainer = styled.div`
+  & > :last-child {
+    margin-left: 1.2em;
+  }
+`
