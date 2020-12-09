0dfb9b9e1699b97445d9f61336dd8f4180bb1546
Rafael Makaha <rafael.makaha@gmail.com>
2020-12-09 03:46:43 -0300

    Update ProductList page
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- app/src/screens/ProductList/index.js
+++ app/src/screens/ProductList/index.js
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAllProducts } from '../../services';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+
+import { ButtonsWrapper, Container, TableWrapper, Typography } from './style';
+
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    getAllProducts()
+      .then(setProducts)
+      .catch(console.log)
+  },[]);
+
   return(
-    <div>ProductList</div>
+    <Container>
+      <Typography variant="h3" gutterBottom>
+          Welcome!
+      </Typography>
+      <ButtonsWrapper>
+        <Button variant="contained" color="primary" onClick={() => history.push('/product')}>
+          New Product
+        </Button>
+      </ButtonsWrapper>
+      <TableWrapper>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Product </TableCell>
+                <TableCell align="right">Description</TableCell>
+                <TableCell align="right">Price</TableCell>
+                <TableCell align="right"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map((row) => (
+                <TableRow key={row?.name}>
+                  <TableCell component="th" scope="row">
+                    {row?.name}
+                  </TableCell>
+                  <TableCell align="right">{row?.description}</TableCell>
+                  <TableCell align="right">{row?.price}</TableCell>
+                  <TableCell align="right">
+                    <Button variant="contained" color="primary" onClick={() => history.push(`/product/${row.id}`)}>
+                      Editar
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </TableWrapper>
+    </Container>
   )
 }
 
---
+++ app/src/screens/ProductList/style.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { Typography as MaterialTypography } from '@material-ui/core';
+
+
+export const TableWrapper = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: auto;
+`
+export const ButtonsWrapper = styled.div`
+  text-align: right;
+  width: 100%;
+  max-width: 1200px;
+  margin: auto;
+  margin-bottom: 1.2em;
+`
+
+export const Container = styled.div`
+  margin-top: 1.2em;
+`
+
+export const Typography = styled(MaterialTypography)`
+  margin-top: 1.2em;
+  text-align: center;
+`
