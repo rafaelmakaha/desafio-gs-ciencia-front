b26213766e97412ec340e1b13a473dde7c5f8490
Rafael Makaha <rafael.makaha@gmail.com>
2020-12-09 03:46:16 -0300

    Add services folder with backend service
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ app/src/services/backend.js
@@ -0,0 +1,72 @@
+import { BACKEND } from '../settings/endpoints'
+import { validFetch } from '../utils'
+
+const common_header = {
+  "Content-Type": "application/json"
+}
+
+export const getAllProducts = () => {
+  const url = BACKEND.baseRoute + BACKEND.product.list.route;
+  const method = BACKEND.product.list.method;
+  return new Promise((resolve, reject) => {
+    fetch(url, { method, headers: common_header })
+    .then(validFetch)
+    .then(resolve)
+    .catch(reject)
+  })
+}
+
+export const createProduct = (product) => {
+  console.log(BACKEND.baseRoute)
+  const url = BACKEND.baseRoute + BACKEND.product.create.route;
+  const method = BACKEND.product.create.method;
+  return new Promise((resolve, reject) => {
+    console.log(JSON.stringify(product))
+    fetch(url,{
+      method,
+      headers: common_header,
+      body: JSON.stringify(product)
+    })
+    .then(validFetch)
+    .then(resolve)
+    .catch(reject)
+  })
+}
+
+
+export const getProductById = ({id}) => {
+  const url = BACKEND.baseRoute + BACKEND.product.getOne.route(id);
+  const method = BACKEND.product.getOne.method;
+  return new Promise((resolve, reject) => {
+    fetch(url,{ method, headers: common_header })
+    .then(validFetch)
+    .then(resolve)
+    .catch(reject)
+  })
+}
+
+export const deleteProductById = ({id}) => {
+  const url = BACKEND.baseRoute + BACKEND.product.delete.route(id);
+  const method = BACKEND.product.delete.method;
+  return new Promise((resolve, reject) => {
+    fetch(url,{ method, headers: common_header })
+    .then(validFetch)
+    .then(resolve)
+    .catch(reject)
+  })
+}
+
+export const updateProduct = ({id, ...product}) => {
+  const url = BACKEND.baseRoute + BACKEND.product.update.route(id);
+  const method = BACKEND.product.update.method;
+  return new Promise((resolve, reject) => {
+    fetch(url,{
+      method,
+      headers: common_header,
+      body: JSON.stringify(product)
+    })
+    .then(validFetch)
+    .then(resolve)
+    .catch(reject)
+  })
+}
